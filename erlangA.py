0ae5237173887cee19aba954566298a196234a64
Haroith <mitasov.home@gmail.com>
2025-04-25 20:13:53 +0500

    -(add) - added  the main program for ErlangA forecast.

--- erlangA.py
+++ erlangA.py
@@ -55,9 +55,51 @@ class ErlangA :
             j += 1
         return 1 + eternal_sum
     
-    def ProbabilityOfAbandon(self, n) : 
+    def probability_of_abandon(self, n) : 
         ro = self.__load(n) # load per agent
         probability_of_abandon = 1/(ro * self.__a(n*self.__mu/self.__O , self.__lambda/self.__O)) + 1 - 1/ro
         return probability_of_abandon
     
+# We know calls forecast
+# We want to achieve service level
+# How many agents we need?
+# Let's make functions for 15 minutes interval
+# Using ErlangA formulae
+# It's ErlangC that works with abandons
 
+calls_forecast_15_min = 147 # 47 calls per 15 minutes
+service_level_percents_goal = 80 # 80%
+service_level_time_goal = 20 # 20 seconds
+# convert to minutes
+service_level_time_goal_minutes = service_level_time_goal/60
+
+# ErlangA formulae work with queue, so SL=80%/20seconds.
+
+# We need to know more statistics
+average_handling_time_seconds = 120 # average handling time in seconds
+average_patience = 10 # average patience
+
+# First step - to find agents without abandons
+erlang_a = ErlangA(calls_forecast_15_min, average_handling_time_seconds, average_patience)
+calculated_sl = 1
+agents = math.ceil(erlang_a.a)
+while calculated_sl < service_level_percents_goal :
+    agents += 1 
+    calculated_sl = erlang_a.service_level_percents(agents, service_level_time_goal_minutes)
+
+# Second step - to find abandons
+calculated_pab = erlang_a.probability_of_abandon(agents)
+print('required agents=' , agents , '.')
+print('resulted probability of abandon=' , calculated_pab*100 ,'%') # about 5% - very few
+
+# Last step - to find agents with abandons
+calls_forecast_15_min = calls_forecast_15_min*(1-calculated_pab)
+erlang_a = ErlangA(calls_forecast_15_min, average_handling_time_seconds, average_patience)
+calculated_sl = 1
+agents = math.ceil(erlang_a.a)
+while calculated_sl < service_level_percents_goal :
+    agents += 1
+    calculated_sl = erlang_a.service_level_percents(agents, service_level_time_goal_minutes)
+
+print('required agents=',agents,'.')
+print('resulted service level=',calculated_sl,'%')
